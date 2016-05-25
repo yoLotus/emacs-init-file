1c948d6e43e9a751e38f7c16f42dad5c9ec27abc
Etienne Bazin <etienne.bazin@wifirst.fr>
2016-05-25 14:13:21 +0200

    add packages and tide configuration

--- init.el
+++ init.el
@@ -29,6 +29,12 @@
     comment-dwim-2			; comment line or end of line
     dockerfile-mode			; mode to edit Dockerfile
     org-bullets				; prettiers headers for org mode
+    ruby-tools				; helpers for ruby
+    typescript-mode			; typescript mode
+    tide				; make typescript easier
+    json-mode				; yes handle json is prety convenient
+    vagrant				; I should start to use is
+    zencoding-mode			; code html with style
     ) "important package to install")
 
 ;; check if packages are installed and install them if not
@@ -145,3 +151,38 @@
 ;; hooks
 (add-hook 'ruby-mode-hook (lambda () "active ruby tools for ruby mode"
 			    (ruby-tools-mode 1)))
+
+
+;; tide (typescript)
+;; sample config
+(add-hook 'typescript-mode-hook
+          (lambda ()
+            (tide-setup)
+            (flycheck-mode +1)
+            (setq flycheck-check-syntax-automatically '(save mode-enabled))
+            (eldoc-mode +1)
+            ;; company is an optional dependency. You have to
+            ;; install it separately via package-install
+            (company-mode-on)))
+
+;; aligns annotation to the right hand side
+(setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+;; format options
+(setq tide-format-options '(:insertSpaceAfterFunctionKeywordForAnonymousFunctions t :placeOpenBraceOnNewLineForFunctions nil))
+;; see https://github.com/Microsoft/TypeScript/blob/cc58e2d7eb144f0b2ff89e6a6685fb4deaa24fde/src/server/protocol.d.ts#L421-473 for the full list available options
+
+;; Tide can be used along with web-mode to edit tsx files
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (tide-setup)
+              (flycheck-mode +1)
+              (setq flycheck-check-syntax-automatically '(save mode-enabled))
+              (eldoc-mode +1)
+              (company-mode-on))))
